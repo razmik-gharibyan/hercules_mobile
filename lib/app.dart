7606665961b026f988332e0a6c645ad9b7b1c610
razmik-gharibyan <razmik.gharibyan.olymp@gmail.com>
2023-10-16 15:31:05 +0400

    Move landing

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hercules_mobile/app/di/inject_dependencies.dart';
 import 'package:hercules_mobile/core/service/navigation_service.dart';
-import 'package:hercules_mobile/feature/landing_screen.dart';
+import 'package:hercules_mobile/feature/landing/landing_screen.dart';
 
 class App extends StatelessWidget {
   App({Key? key}) : super(key: key);
